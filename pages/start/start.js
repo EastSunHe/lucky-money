a647cc687766e9799e5c8701360a7b951262c96e
idoosmart <45999735+idoosmart@users.noreply.github.com>
2019-04-09 23:42:57 +0800

    创建lucky money工程

---
+++ pages/start/start.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+    // text:"这是一个页面"
+  },
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({ title: '发红包' }); 
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
